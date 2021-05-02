c6a3dda9f1101dff5607d9bef37aa972579b0c82
Wei <wei@weis-mbp.lan>
2021-05-02 00:46:04 -0400

    add test data

--- crypto-site/src/App.js
+++ crypto-site/src/App.js
@@ -1,34 +1,36 @@
 import "./App.css";
 import "antd/dist/antd.css";
 
-import { Row, Col, Layout, Menu } from "antd";
-const { Header, Sider, Content } = Layout;
+import { Route, Link } from "react-router-dom";
+import { Layout, Menu } from "antd";
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
     <Layout>
-      <Header
-        style={{
-          position: "fixed",
-          zIndex: 1,
-          width: "100%",
-        }}
-      >
+      <Header>
         <div className="logo" />
 
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
+        <Menu theme="dark" mode="horizontal" >
+          <Menu.Item key="1"> <Link to='/market'>Market</Link></Menu.Item>
+          <Menu.Item key="2"> <Link to='/news'>News</Link></Menu.Item>
         </Menu>
       </Header>
+
       <Content
         className="site-layout"
-        style={{ padding: "0 50px", marginTop: 64, background: "gray" }}
+        style={{ padding: "0 50px", marginTop: 10 }}
       >
-        <p>Content</p>
+        <Route path="/market">
+          <div className="site-layout-content">Market</div>
+        </Route>
+        <Route path="/news">
+          <div className="site-layout-content">News</div>
+        </Route>
       </Content>
-      {/* <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer> */}
+      <Footer style={{ textAlign: "center" }}>
+        Ant Design ©2018 Created by Ant UED
+      </Footer>
     </Layout>
   );
 }
