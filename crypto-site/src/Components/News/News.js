eba264c14373878957e9c8fb923a3bca23b9a401
weimbp <wchen8878@gmail.com>
2021-05-06 14:58:12 -0400

    News Page conponent in process

--- crypto-site/src/Components/News/News.js
+++ crypto-site/src/Components/News/News.js
@@ -1,3 +1,5 @@
+import NewsBar from './NewsBar'
+
 const News = (props) => {
     return <p>This is news Page</p>
 }
---
+++ crypto-site/src/Components/News/NewsBar.js
@@ -0,0 +1,11 @@
+import { Image } from 'antd';
+
+
+const NewsBar = (props) => {
+    return
+        <>
+        </>
+    
+}
+
+export default NewsBar;
