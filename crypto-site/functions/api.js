e9aded6ffd8985fe7cb280794cfaef767a0607c3
Chen Wei <wchen8878@gmail.com>
2021-05-31 22:21:07 -0400

    basic connection between faunadb and netlify completed

--- crypto-site/functions/api.js
+++
@@ -1,9 +0,0 @@
-const getGraphData = require("./helper/getGraphData")
-
-exports.handler = async (event) => {
-    if (event.httpMethod === 'GET') {
-        return await getGraphData(event);
-    }  else {
-        return formattedReturn(405, {});
-    }
-};
